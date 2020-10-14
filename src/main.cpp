6daee1024608e23a4ab5716f72dcabde1916a5ab
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2020-10-14 17:12:08 -0300

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <Wire.h>
+void setup() {
+    Serial.begin(115200);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+    delay(1000);
+    Serial.print("Ola\n");
+  // put your main code here, to run repeatedly:
+}
