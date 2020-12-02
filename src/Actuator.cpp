1c233d09f85a7b724047a1aecc4abf04c244980d
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2020-12-02 09:13:29 -0300

    removed compatible with arduino uno

--- src/Actuator.cpp
+++ src/Actuator.cpp
@@ -10,43 +10,40 @@ Actuator::Actuator(uint8_t en, uint8_t dir1, uint8_t dir2)
 
 void Actuator::init()
 {
-    #if defined(ESP)
-        ledcSetup(0, 10000, 8);
-        ledcAttachPin(_en, 0);
-    #else
-        pinMode(_en, OUTPUT);
-    #endif
+    ledcSetup(0, 5000, 8);
+    ledcAttachPin(_en, 0);
     pinMode(_dir1, OUTPUT);
     pinMode(_dir2, OUTPUT);
     this->setPower(0);
 };
 //ledcAttachPin(ledPin, ledChannel);
-    
+
 void Actuator::setPower(float power)
 {
     if (power < 0)
     {
-        #if defined(ESP)
-            ledcWrite(0, -(power)*255);
-        #else
-            analogWrite(_en, -(power)*255);
-        #endif
+#if defined(ESP)
+        ledcWrite(0, -(power)*255);
+#else
+        analogWrite(_en, -(power)*255);
+#endif
         digitalWrite(_dir1, HIGH);
         digitalWrite(_dir2, LOW);
     }
     else
     {
-        #if defined(ESP)
-            ledcWrite(0, (power)*255);
-        #else
-            analogWrite(_en, (power)*255);
-        #endif
+#if defined(ESP)
+        ledcWrite(0, (power)*255);
+#else
+        analogWrite(_en, (power)*255);
+#endif
         digitalWrite(_dir1, LOW);
         digitalWrite(_dir2, HIGH);
     }
     _power = power;
 }
 
-float Actuator::getPower(){
+float Actuator::getPower()
+{
     return _power;
 }
