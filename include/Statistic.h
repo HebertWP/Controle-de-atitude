81588a021fd2dbe8057c4e69f11ac41637f3d05d
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2021-01-02 22:46:29 -0300

    Coments added in Statistic library

--- include/Statistic.h
+++ include/Statistic.h
@@ -2,10 +2,26 @@
 #define __Statistic_H__
 #include <Arduino.h>
 
+/**
+ * @brief Interface that request to implement statistic to some sensor 
+ * 
+ */
 class Statistic
 {
 public:
+
+    /**
+     * @brief get mean last sample recored by this sensor 
+     * 
+     * @return float return the mean of measurement 
+     */
     virtual float mean();
+
+    /**
+     * @brief variance of last measurement set 
+     * 
+     * @return float return de variance os measurement 
+     */
     virtual float variance();
 };
 
