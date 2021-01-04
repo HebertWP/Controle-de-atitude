a835859f390b92044a0101da612441f069420f9f
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2021-01-04 03:17:00 -0300

    Sensor Library comments added

--- include/Sensor.h
+++ include/Sensor.h
@@ -2,19 +2,53 @@
 #define __Sensor_H__
 #include <Arduino.h>
 
+/**
+ * @brief Interface to sample reding to sensors 
+ * 
+ */
 class Sensor
 {
 public:
+
+    /**
+     * @brief star read samples
+     * 
+     */
     virtual void starRead();
 
+    /**
+     * @brief call it during measure to proper measure the sensor 
+     * 
+     */
     virtual void reading();
 
+    /**
+     * @brief ask if the sample is done
+     * 
+     * @return true if the measure is over
+     * @return false you need call reading() more times 
+     */
     virtual bool isReadDone();
 
+    /**
+     * @brief get samples array 
+     * 
+     * @return float* poiter to first measure in time 
+     */
     virtual float *read();
 
+    /**
+     * @brief Set the Num Samples in measure
+     * 
+     * @param num number of samples
+     */
     virtual void setNumSamples(uint16_t num);
 
+    /**
+     * @brief Set the Freq of aquisition(remebe nyquist)
+     * 
+     * @param freq 
+     */
     virtual void setFreq(uint16_t freq);
 };
 
